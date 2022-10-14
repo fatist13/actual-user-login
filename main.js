8ee330817972dc703630dd05e4ece6d81ed26a3e
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-14 14:48:26 -0600

    l

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ function btnclicked() {
 let user = document.getElementById("user");
 let pass = document.getElementById("pass");
 
-if (user != "admin" && pass != "1234") {
+if (user !== "admin" && pass !== "1234") {
 alert("Login Unsuccessful");
-} 
+} else if (user === "admin" && pass === "1234") {
+    alert("Login Successful");
 }
-
+}
 
