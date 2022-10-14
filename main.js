46e77a4fd9a5b10a96fadb2d045cf056b8463507
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-14 13:59:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // JavaScript Code
+document.getElementById("btn").addEventListener("click", btnclicked);
+
+function btnClicked() {
+let user = document.getElementById("user");
+let pass = document.getElementById("pass");
+
+if 
+
+
+
+
+}
