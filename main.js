3556272bc56ccaa0443d6095b0a528e6c6416445
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-17 14:37:12 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ document.getElementById("user");
 document.getElementById("pass");
 
 function btnclicked() {
-    let user = document.getElementById("user");
-    let pass = document.getElementById("pass");
+    let user = document.getElementById("user").value.toLowerCase();
+    let pass = document.getElementById("pass").value.toLowerCase();
     if (user === "admin" && pass === "1234") {
        alert("Login Successful");
     } else {
