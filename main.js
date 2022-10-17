ec951b31bac2e85ccc265bd6e5f8587549962a6c
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-17 14:34:43 -0600

    l

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 // JavaScript Code
 document.getElementById("btn").addEventListener("click", btnclicked);
+document.getElementById("user");
+document.getElementById("pass");
 
 function btnclicked() {
     let user = document.getElementById("user");
     let pass = document.getElementById("pass");
     if (user === "admin" && pass === "1234") {
        alert("Login Successful");
+    } else {
+        alert("Login Unsuccessful");
     }
 }
 
 
-function btnclicked() {
-     let user = document.getElementById("user");
-     let pass = document.getElementById("pass");
-     if (user != "admin" && pass != "1234") {
-        alert("Login Unsuccessful");
-     }
-}
