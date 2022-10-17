eb422ce72cabb20a20ae10d3873186f7a3368e3c
b.anwar <109045924+fatist13@users.noreply.github.com>
2022-10-17 14:20:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,18 @@
 document.getElementById("btn").addEventListener("click", btnclicked);
 
 function btnclicked() {
-let user = document.getElementById("user");
-let pass = document.getElementById("pass");
-
-if (user == "admin" && pass == "1234") {
-alert("Login Successful");
-} else if (user != "admin" && pass != "1234") {
-    alert("Login Unuccessful");
-}
+    let user = document.getElementById("user");
+    let pass = document.getElementById("pass");
+    if (user === "admin" && pass === "1234") {
+       alert("Login Successful");
+    }
 }
 
+
+function btnclicked() {
+     let user = document.getElementById("user");
+     let pass = document.getElementById("pass");
+     if (user != "admin" && pass != "1234") {
+        alert("Login Unsuccessful");
+     }
+}
